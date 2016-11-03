4ec9d39c471646e68287f886d908f8bcbf031ad4
MarkusSchulze <markus-schulze@gmx.net>
2016-11-03 15:34:52 +0100

    first running step

--- app/src/main/java/hci/smartwatchmouse/MainActivity.java
+++ app/src/main/java/hci/smartwatchmouse/MainActivity.java
@@ -2,46 +2,63 @@ package hci.smartwatchmouse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import java.util.List;
 import android.util.Log;
+import java.util.List;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
 
-public class MainActivity extends Activity
+public class MainActivity extends WearableActivity
         implements SensorEventListener{
 
-    private TextView mTextView;
     private Sensor accSensor;
-    private TextView accView;
     private SensorManager sensorManager;
+    private TextView sensorX,sensorY, sensorZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : sensorList) {
+            Log.d("MainActivity", sensor.toString());
+        }
 
-        setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
-        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); sensorManager.registerListener(this, accSensor, 100 * 1000);
-        accView = (TextView) findViewById(R.id.line1);
+        setAmbientEnabled();
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accSensor, 100 * 1000);
+
+        sensorX = (TextView) findViewById(R.id.sensor_X);
+        sensorY = (TextView) findViewById(R.id.sensor_Y);
+        sensorZ = (TextView) findViewById(R.id.sensor_Z);
+
+
+//        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+//        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+//            @Override
+//            public void onLayoutInflated(WatchViewStub stub) {
+//                //mTextView = (TextView) stub.findViewById(R.id.sensor_Y);
+//            }
+//        });
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) { Log.d("MainActivity", "onSensorChanged: " + event); if (event.sensor == accSensor) {
-        float[] vs = event.values;
-        accView.setText(String.format("a: %.3f, %.3f, %.3f", vs[0], vs[1], vs[2])); }
+    public void onSensorChanged(SensorEvent event) {
+        Log.d("MainActivity", "onSensorChanged: " + event);
+        if (event.sensor == accSensor) {
+            float[] vs = event.values;
+            //sensorY.setText(String.format("a: %.3f, %.3f, %.3f", vs[0], vs[1], vs[2]));
+            //sensorX.setText("Test234");
+        }
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) { Log.d("MainActivity", "onAccuracyChanged: " + sensor + ", " + accuracy);
