b5a6acdbfcab96eb8377bfad0569e7744d2e7215
moxgit <moxmosi@gmail.com>
2016-11-24 14:34:24 +0100

    Merge branch 'master' of Z:\Dropbox\Master\4_WiSe16-17\Labor_Aktuelle_Themen_der_MCI\SmartwatchMouse with conflicts.

--- app/src/main/java/hci/smartwatchmouse/MainActivity.java
+++ app/src/main/java/hci/smartwatchmouse/MainActivity.java
@@ -1,37 +1,67 @@
 package hci.smartwatchmouse;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.support.wearable.view.WatchViewStub;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.util.Log;
-import java.util.List;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends WearableActivity
-        implements SensorEventListener{
+        implements SensorEventListener {
 
+    private static final int SCAN_PERIOD = 5;
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothManager bm;
+    BluetoothAdapter ba;
+    BluetoothLeScanner bs;
+    Handler handler = new Handler();
+    Handler mHandler = new Handler();
+    boolean mScanning = true;
     private Sensor accSensor, gyroSensor, accCleanSensor, gameRotationSensor;
+
+    // Bluetooth LE ------------------------------------------------------------
     private SensorManager sensorManager;
-    private TextView sensorX,sensorY, sensorZ;
-    private String accData, gyroData, gameData;
-    private Button BigB;
+    private TextView sensorX, sensorY, sensorZ;
+    private boolean isBluetooth_LE = true;
+    private BluetoothAdapter mBluetoothAdapter;
+    private ScanCallback mLeScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
         for (Sensor sensor : sensorList) {
             Log.d("MainActivity", sensor.toString());
@@ -48,50 +78,107 @@ public class MainActivity extends WearableActivity
         sensorManager.registerListener(this, accCleanSensor, 100 * 1000);
         sensorManager.registerListener(this, gameRotationSensor, 100 * 1000);
 
+
         sensorX = (TextView) findViewById(R.id.sensor_X);
         sensorY = (TextView) findViewById(R.id.sensor_Y);
         sensorZ = (TextView) findViewById(R.id.sensor_Z);
-        BigB = (Button) findViewById(R.id.BigButton);
 
-        BigB.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Click();
-            }
-        });
+        // final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        //  stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+        //  @Override
+        //      public void onLayoutInflated(WatchViewStub stub) {
+        //          mTextView = (TextView) stub.findViewById(R.id.sensor_Y);
+        //      }
+        //  });
+
+        // Bluetooth LE ------------------------------------------------------------
+
+        // Bluetooth Check: Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            isBluetooth_LE = false;
+            finish();
+        }
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
     }
 
-    public void Click(){
-        Log.d("Game", "------------");
-    }
-
-    @Override
+   @Override
     public void onSensorChanged(SensorEvent event) {
-        //Log.d("MainActivity", "onSensorChanged: " + event);
-        if (event.sensor == gyroSensor){
+        Log.d("MainActivity", "onSensorChanged: " + event);
+        if (event.sensor == gyroSensor) {
             float[] v = event.values;
-            sensorX.setText(String.format("%+4.3f, %+4.3f, %+4.3f", v[0], v[1], v[2]));
-            //Log.d("Gyro", String.format("%.3f, %.3f, %.3f", v[0], v[1], v[2]));
+            sensorX.setText(String.format("a: %.3f, %.3f, %.3f", v[0], v[1], v[2]));
         }
         if (event.sensor == accSensor) {
             float[] vs = event.values;
-            sensorY.setText(String.format("%+4.3f, %+4.3f, %+4.3f", vs[0], vs[1], vs[2]));
-            //Log.d("Acc", String.format("%.3f, %.3f, %.3f", vs[0], vs[1], vs[2]));
-
-
+            sensorY.setText(String.format("a: %.3f, %.3f, %.3f", vs[0], vs[1], vs[2]));
         }
-        if (event.sensor == gameRotationSensor){
+        if (event.sensor == gameRotationSensor) {
             float[] vx = event.values;
-            sensorZ.setText(String.format("%+4.3f, %+4.3f, %+4.3f", vx[0], vx[1], vx[2]));
-            Log.d("Game", String.format("%.3f, %.3f, %.3f", vx[0], vx[1], vx[2]));
-
+            sensorZ.setText(String.format("a: %.3f, %.3f, %.3f", vx[0], vx[1], vx[2]));
         }
     }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         Log.d("MainActivity", "onAccuracyChanged: " + sensor + ", " + accuracy);
     }
-    /*public void onClose() {
-        Log.d("MainActivity","onClose");
 
+    /*
+    public void bluetoothLE() {
+        bm = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        ba = bm.getAdapter();
+        bs = ba.getBluetoothLeScanner(); // API 21
+        ...
+        Log.d("MainActivity", "version: " + android.os.Build.VERSION.SDK_INT);
+        handler.postDelayed(new Runnable() {
+            public void run() {...}
+        }, 10000); // turn scanning off after 10 seconds
+        if (android.os.Build.VERSION.SDK_INT < 21) {
+            ba.startLeScan(bleStartScan);
+        } else {
+            bs.startScan(sc);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BluetoothManager bluetoothManager = (BluetoothManager) getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        final BluetoothLeScanner bluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    bluetoothLeScanner.stopScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            bluetoothLeScanner.startScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            bluetoothLeScanner.stopScan(mLeScanCallback);
+        }
     }*/
 }
